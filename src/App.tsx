c44bfc4f0bc36271f5bb8df2c8590dd2d6598e78
luisfilipemendonca <filipedev@outlook.pt>
2025-04-26 08:42:29 -0700

    Added categories to the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "./app/hooks";
 import TaskGrid from "./features/tasks/TaskGrid";
 import Header from "./layout/Header";
 import Main from "./layout/Main";
 import Navbar from "./layout/Navbar";
 
+import { getCategories } from "./features/categories/categoriesSlice";
+
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
   return (
     <div className="h-full">
       <Header />
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,31 +1,50 @@
+import { useAppSelector } from "../app/hooks";
 import CategoriesCreateButton from "../features/categories/CategoriesCreateButton";
+import {
+  categoriesSelector,
+  getCategoriesStatus,
+} from "../features/categories/categoriesSlice";
 
 const Navbar = () => {
+  const categories = useAppSelector(categoriesSelector);
+  const categoriesStatus = useAppSelector(getCategoriesStatus);
+
+  const renderLoading = () => <p className="text-center">Loading categories</p>;
+
+  const renderEmptyState = () => (
+    <p className="text-center">No categories created yet</p>
+  );
+
+  const renderCategories = () => {
+    return (
+      <nav>
+        <ul>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <button className="w-full hover:bg-primary-400 cursor-pointer border-1 border-primary-500 rounded-sm bg-primary-400 text-primary-100 mb-8 py-2 text-center text-xl">
+                {category.description}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  };
+
+  const renderContent = () => {
+    if (categoriesStatus === "loading") return renderLoading();
+    if (!categories.length) return renderEmptyState();
+
+    return renderCategories();
+  };
+
   return (
     <div
       className="fixed w-60 py-8 px-4 bg-primary-200 border-r-2 border-r-primary-400 flex flex-col justify-between"
       style={{ height: "calc(100% - 60px)" }}
     >
-      <nav>
-        <ul>
-          <li>
-            <button className="w-full hover:bg-primary-400 cursor-pointer border-1 border-primary-500 rounded-sm bg-primary-400 text-primary-100 mb-8 py-2 text-center text-xl">
-              Primeiro
-            </button>
-          </li>
-          <li>
-            <button className="w-full hover:bg-primary-400 cursor-pointer border-1 border-primary-500 rounded-sm bg-primary-300 text-primary-100 mb-8 py-2 text-center text-xl">
-              Segundo
-            </button>
-          </li>
-          <li>
-            <button className="w-full hover:bg-primary-400 cursor-pointer border-1 border-primary-500 rounded-sm bg-primary-300 text-primary-100 py-2 text-center text-xl">
-              Terceiro
-            </button>
-          </li>
-        </ul>
-      </nav>
-      <CategoriesCreateButton />
+      {renderContent()}
+      {categoriesStatus !== "loading" && <CategoriesCreateButton />}
     </div>
   );
 };
